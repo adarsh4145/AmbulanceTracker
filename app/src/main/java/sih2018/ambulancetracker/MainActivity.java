f198e114259cf3b3d4b05c051c247ecac9456ff6
Adarsh Kumar Pandey <adarshkumar4145@gmail.com>
2018-02-04 19:09:29 +0530

    Initial commit

---
+++ app/src/main/java/sih2018/ambulancetracker/MainActivity.java
@@ -0,0 +1,13 @@
+package sih2018.ambulancetracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
